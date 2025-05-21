c3b80f17a2be2ed391745df0ba9e3b0eaa4f6acb
John Luis Pepito <luis.pepito789@gmail.com>
2025-05-21 16:40:31 +0800

    Main

---
+++ portfolioX-backend/src/main/java/cit/edu/portfolioX/PortfolioXApplication.java
@@ -0,0 +1,13 @@
+package cit.edu.portfolioX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioXApplication.class, args);
+	}
+
+}
